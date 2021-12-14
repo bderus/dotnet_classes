3bf18b60886482bf8e31ffbadcd2231cab4ad43d
Yoshimo94 <maciekprzygoda@interia.pl>
2021-12-14 19:01:48 +0100

    Lipton - Hardcorowe pierwsze kroki w C#...:(

--- TicTacToe/TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToe/TicTacToeGame.cs
@@ -39,25 +39,70 @@ namespace TicTacToe
                 if (CheckEndgameConditions())//if the method returns true -> do whatever is inside of the IF block
                 {
                     //TODO - End the game here
-                }
 
-                SwitchCurrentPlayer();
+
+                    SwitchCurrentPlayer();
             }
         }
 
         private void SwitchCurrentPlayer()
         {
-            //TODO - Implement method which switches current player (from "X" to "O" and vice versa)
+                //TODO - Implement method which switches current player (from "X" to "O" and vice versa)
+                if (CurrentPlayer == "X")
+                    CurrentPlayer = "0";
+                else if (CurrentPlayer == "0")
+                    CurrentPlayer = "X";
 
             throw new NotImplementedException();
         }
 
         private bool CheckEndgameConditions()
         {
-            //TODO - Implement method which checks endgame conditions - draw and win
-            //TODO - This method returns bool <- it is used in the Play() method to check if the game should be ended
+                //TODO - Implement method which checks endgame conditions - draw and win
+                if (_board[0][0] == _board[0][1] == _board[0][2] == CurrentPlayer)
+                {
+                    return true;
+                }
 
-            throw new NotImplementedException();
+                if (_board[1][0] == _board[1][1] == _board[1][2] == CurrentPlayer)
+                {
+                    return true;
+                }
+
+                if (_board[2][0] == _board[2][1] == _board[2][2] == CurrentPlayer)
+                {
+                    return true;
+                }
+                if (_board[0][0] == _board[1][0] == _board[2][0] == CurrentPlayer)
+                {
+                    return true;
+                }
+                if (_board[0][1] == _board[1][1] == _board[2][1] == CurrentPlayer)
+                {
+                    return true;
+                }
+                if (_board[0][2] == _board[1][2] == _board[2][2] == CurrentPlayer)
+                {
+                    return true;
+                }
+                if (_board[0][0] == _board[1][1] == _board[2][2] == CurrentPlayer)
+                {
+                    return true;
+                }
+                if (_board[2][0] == _board[1][1] == _board[0][2] == CurrentPlayer)
+                {
+                    return true;
+                }
+                
+
+                foreach (var item in _board)
+                    foreach (var item2 in item)
+                        if (item2 != "*")
+                            return false;
+                //TODO - This method returns bool <- it is used in the Play() method to check if the game should be ended
+                return true;
+
+                throw new NotImplementedException();
         }
 
         private void PlayerChoice()
@@ -75,8 +120,43 @@ namespace TicTacToe
                 _board[0][1] = CurrentPlayer;
 
             }
-            //TODO - Implement missing player selections; *Extra - Implement user input validation
-            PrintBoard();
+            if (userInput == "3")
+            {
+                _board[0][2] = CurrentPlayer;
+
+            }
+            if (userInput == "4")
+            {
+                _board[1][0] = CurrentPlayer;
+
+            }
+            if (userInput == "5")
+            {
+                _board[1][1] = CurrentPlayer;
+
+            }
+            if (userInput == "6")
+            {
+                _board[1][2] = CurrentPlayer;
+
+            }
+            if (userInput == "7")
+            {
+                _board[2][0] = CurrentPlayer;
+
+            }
+            if (userInput == "8")
+            {
+                _board[2][1] = CurrentPlayer;
+
+            }
+            if (userInput == "9")
+            {
+                _board[2][2] = CurrentPlayer;
+
+            }
+                //TODO - Implement missing player selections; *Extra - Implement user input validation
+                PrintBoard();
         }
         
 

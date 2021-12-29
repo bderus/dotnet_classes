5d8effae6f8b1c3ff69290b754af403a677df582
Yoshimo94 <maciekprzygoda@interia.pl>
2021-12-29 12:39:05 +0100

    implemented ToDoListController contructor

--- ToDoApp/ToDoApp/BusinessLogic/Controllers/ToDoListController.cs
+++ ToDoApp/ToDoApp/BusinessLogic/Controllers/ToDoListController.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoApp.Models;
 
 namespace ToDoApp.BusinessLogic.Controllers
 {
     internal class ToDoListController
     {
+        private int CurrentListId { get; set; }
+        private List<ToDoList> ToDoLists { get; set; }
+        private ToDoItemController ToDoItemController { get; set; }
+
+        public ToDoListController()
+        {
+            CurrentListId = 1;
+            ToDoLists = new List<ToDoList>();
+            ToDoItemController = new ToDoItemController();
+        }
+
+
+
     }
 }

dc353a15773f6cad221e238d42bb1645687c16c7
Yoshimo94 <maciekprzygoda@interia.pl>
2021-12-29 11:01:33 +0100

    KDP_13 Done

---
+++ ToDoApp/ToDoApp/BusinessLogic/Controllers/ToDoItemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.BusinessLogic.Controllers
+{
+    internal class ToDoItemController
+    {
+        private int CurrentId { get; set; }
+
+        public ToDoItemController()
+        { 
+            CurrentId = 1;
+        }
+            
+    }
+}

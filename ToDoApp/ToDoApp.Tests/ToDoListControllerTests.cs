76aa1839aa2ee4e880a8a44983695d1f1f960c77
Yoshimo94 <maciekprzygoda@interia.pl>
2022-01-13 21:22:42 +0100

    Added tests for CreateNewToDoLists method

--- ToDoApp/ToDoApp.Tests/ToDoListControllerTests.cs
+++ ToDoApp/ToDoApp.Tests/ToDoListControllerTests.cs
@@ -1,9 +1,63 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoApp.BusinessLogic.Controllers;
+using ToDoApp.Models;
+using System.Collections.Generic;
 
 namespace ToDoApp.Tests
 {
     [TestClass]
     public class ToDoListControllerTests
+      
     {
+        private readonly ToDoListController _testListController = new ToDoListController();
+
+        [TestMethod]
+
+        public void CreateNewToDoList_ToDoList_ShouldBeCreated()
+        {
+            //Arrange & Act
+            var actualList = _testListController.CreateNewToDoList(null);
+      
+            //Assert
+
+            Assert.IsInstanceOfType(actualList, typeof(ToDoList));
+        }
+
+        [TestMethod]
+        public void CreateNewToDoList_ToDoList_ShouldBeCreated_WithProperNameList()
+        {
+            //Arrange
+            var expectedListName = "TestListName";
+
+            //Act
+            var actualList = _testListController.CreateNewToDoList(expectedListName);
+
+            //Assert
+
+            Assert.AreEqual(expectedListName, actualList.Name);
+        }
+        [TestMethod]
+        public void CreateNewToDoList_ToDoList_ShouldBeCreated_WithProperId()
+        {
+            //Arrange
+            var expectedListId = 1;
+
+            //Act
+            var actualList = _testListController.CreateNewToDoList(null);
+
+            //Assert
+            Assert.AreEqual(expectedListId, actualList.Id);
+        }
+
+        [TestMethod]
+
+        public void CreateNewToDoList_ToDoList_ShouldBeCreated_WithProperInsideObjectType()
+        {
+
+            //Arrange & Act
+            var actualListItem = _testListController.CreateNewToDoList(null);
+            //Assert
+            Assert.IsInstanceOfType(actualListItem.ToDoItems, typeof(List<ToDoItem>));
+        }
     }
 }
--- ToDoApp/ToDoApp/Models/ToDoItem.cs
+++ ToDoApp/ToDoApp/Models/ToDoItem.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ToDoApp.Models
 {
-    internal class ToDoItem
+    public class ToDoItem
     {
         public int Id { get; set; }
         public string Name { get; set; }

e272a6128fe871a3efc57fbcc7b687a7d8729e34
barto_der <bartlomiej.derus@gmail.com>
2022-01-24 19:45:24 +0100

    initialize winforms app

---
+++ ToDoApp/ToDoApp.WinForms/Views/MainWindow.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ToDoApp.WinForms.Views
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logInButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // logInButton
+            // 
+            this.logInButton.Location = new System.Drawing.Point(307, 167);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(177, 85);
+            this.logInButton.TabIndex = 0;
+            this.logInButton.Text = "Zaloguj sie";
+            this.logInButton.UseVisualStyleBackColor = true;
+            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.logInButton);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button logInButton;
+    }
+}
---
+++ ToDoApp/ToDoApp.WinForms/Views/MainWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ToDoApp.WinForms.Views
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void logInButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

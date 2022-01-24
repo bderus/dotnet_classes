a9e2f83027d874adafec37aca41a74d98735640b
Yoshimo94 <maciekprzygoda@interia.pl>
2022-01-24 21:28:30 +0100

    Add newList in MainWindow

--- ToDoApp/ToDoApp.WinForms/Views/MainWindow.Designer.cs
+++ ToDoApp/ToDoApp.WinForms/Views/MainWindow.Designer.cs
@@ -28,33 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.logInButton = new System.Windows.Forms.Button();
+            this.nowaLista = new System.Windows.Forms.Button();
+            this.listContainer = new System.Windows.Forms.ListBox();
+            this.nowyItem = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // logInButton
+            // nowaLista
             // 
-            this.logInButton.Location = new System.Drawing.Point(307, 167);
-            this.logInButton.Name = "logInButton";
-            this.logInButton.Size = new System.Drawing.Size(177, 85);
-            this.logInButton.TabIndex = 0;
-            this.logInButton.Text = "Zaloguj sie";
-            this.logInButton.UseVisualStyleBackColor = true;
-            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+            this.nowaLista.Location = new System.Drawing.Point(453, 94);
+            this.nowaLista.Name = "nowaLista";
+            this.nowaLista.Size = new System.Drawing.Size(231, 111);
+            this.nowaLista.TabIndex = 1;
+            this.nowaLista.Text = "nowaLista";
+            this.nowaLista.UseVisualStyleBackColor = true;
+            this.nowaLista.Click += new System.EventHandler(this.newList_Click);
+            // 
+            // listContainer
+            // 
+            this.listContainer.FormattingEnabled = true;
+            this.listContainer.ItemHeight = 20;
+            this.listContainer.Location = new System.Drawing.Point(457, 222);
+            this.listContainer.Name = "listContainer";
+            this.listContainer.Size = new System.Drawing.Size(227, 324);
+            this.listContainer.TabIndex = 2;
+            // 
+            // nowyItem
+            // 
+            this.nowyItem.Location = new System.Drawing.Point(88, 94);
+            this.nowyItem.Name = "nowyItem";
+            this.nowyItem.Size = new System.Drawing.Size(219, 102);
+            this.nowyItem.TabIndex = 3;
+            this.nowyItem.Text = "nowyItem";
+            this.nowyItem.UseVisualStyleBackColor = true;
+            this.nowyItem.Click += new System.EventHandler(this.newItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 268);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 27);
+            this.textBox1.TabIndex = 4;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.logInButton);
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.nowyItem);
+            this.Controls.Add(this.listContainer);
+            this.Controls.Add(this.nowaLista);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button logInButton;
+        private System.Windows.Forms.Button newItem;
+        private System.Windows.Forms.Button nowaLista;
+        private System.Windows.Forms.ListBox listContainer;
+        private System.Windows.Forms.Button nowyItem;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- ToDoApp/ToDoApp.WinForms/Views/MainWindow.cs
+++ ToDoApp/ToDoApp.WinForms/Views/MainWindow.cs
@@ -1,18 +1,52 @@
 ﻿using System;
 using System.Windows.Forms;
+using ToDoApp.BusinessLogic.Controllers;
 
 namespace ToDoApp.WinForms.Views
 {
     public partial class MainWindow : Form
     {
+        private ToDoListController _listController { get; set; }
+
+        private ToDoItemController _itemController { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            _listController = new ToDoListController();
+            _itemController = new ToDoItemController();
         }
 
-        private void logInButton_Click(object sender, EventArgs e)
-        {
 
+        private void newList_Click(object sender, EventArgs e)
+        {
+            var listName = textBox1.Text;
+            if (listName.Length >= 3)
+            {
+                var newList = _listController.CreateNewToDoList(listName);
+                listContainer.Items.Add(newList.Name);
+            }
+            else
+            {
+                var userAnswer = MessageBox.Show("Nazwa listy jest za krótka", "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (userAnswer == DialogResult.Yes)
+                {
+                    MessageBox.Show("Jesteś super");
+                }
+                else if (userAnswer == DialogResult.No)
+                {
+                    MessageBox.Show("O Ty chuju");
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+
+        }
+
+        private void newItem_Click(object sender, EventArgs e)
+        {
+            var newItem = _itemController.CreateNewToDoItem("itemName");
         }
     }
 }

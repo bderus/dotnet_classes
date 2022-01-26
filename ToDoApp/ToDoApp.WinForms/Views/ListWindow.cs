6fb15bffef9e93c4f230e00f94eb1265d05b7a94
Yoshimo94 <maciekprzygoda@interia.pl>
2022-01-26 19:49:48 +0100

    Add new window option and add new item to itemContainer

---
+++ ToDoApp/ToDoApp.WinForms/Views/ListWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ToDoApp.BusinessLogic.Controllers;
+
+namespace ToDoApp.WinForms.Views
+{
+    public partial class ListWindow : Form
+    {
+        private ToDoItemController _itemController { get; set; }
+        public ListWindow()
+        {
+            InitializeComponent();
+            _itemController = new ToDoItemController();
+        }
+
+        private void newItem_Click(object sender, EventArgs e)
+        {
+            var itemName = textBox1.Text;
+            var newItem = _itemController.CreateNewToDoItem(itemName);
+            itemContainer.Items.Add(newItem.Name);
+        }
+    }
+}

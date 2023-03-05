1d0b934c4afb798987e55ec6d170787a69a201c8
Maciej Przygoda <maciekprzygoda@interia.pl>
2023-03-05 18:01:22 +0100

    Init commit

---
+++ FileOperation/FileOperation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileOperation
+{
+    internal class FileOperation
+    {
+        public string Path { get; set; }
+        public string DestFilePath { get; set; }
+
+        public FileOperation(string path, string destFilePath)
+        {
+            Path = path;
+            DestFilePath = destFilePath;
+        }
+
+        public void RollFile()
+        {
+            var path = Path;
+            var destFilePath = DestFilePath;
+
+            if (CheckIfFileToCopyExist(path))
+            {
+                CopyFile(path, destFilePath);
+            }
+            else
+            {
+                Console.WriteLine("Nie znaleziono pliku o podanej ścieżce");
+            }
+        }
+
+        public bool CheckIfFileToCopyExist(string path)
+        {
+            return File.Exists(path);
+        }
+        public bool CheckIfDestFileExist(string destFilePath)
+        {
+            if (File.Exists(destFilePath))
+            {
+                Console.WriteLine("Plik o nazwie RA juz istnieje");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public void CopyFile(string path, string destFilePath)
+        {
+            if (CheckIfDestFileExist(destFilePath))
+            {
+                File.Copy(path, "RA");
+                Console.WriteLine("Plik został skopionowany");
+            }
+           
+        }
+
+    }
+}
---
+++ FileOperation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new FileOperation(args[0], args[1]);
+            file.RollFile();
+        }
+    }
+}

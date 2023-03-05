15246b5779977df166936567597ee4af4bdb65e7
piotr <piotrekacki@gmail.com>
2023-03-05 19:48:49 +0100

    Implemented CommandLineParser

---
+++ FileOperation/CommandLineParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperation
+{
+    internal class CommandLineParser
+    {
+        public List<string> ArgumentParser(string[] args)
+        {
+            var parsedArgs = new List<string>();
+            foreach(string arg in args)
+            {
+                if (arg.Contains("="))
+                {
+                    arg.Substring(arg.IndexOf('='));
+                    parsedArgs.Add(arg);
+                }
+                else
+                {
+                    Console.WriteLine("Niewłaściwie podany argument");
+                }             
+
+            }
+            return parsedArgs;
+            
+        }
+    }
+}
